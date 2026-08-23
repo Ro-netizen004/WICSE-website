8cf145e1f5bbf32a03d7bf28c705f4c9852ce84f
Ash <115380969+ash3087@users.noreply.github.com>
2026-08-22 23:51:43 -0400

    Update E-Board member information

--- frontend/src/data/teams.js
+++ frontend/src/data/teams.js
@@ -8,7 +8,46 @@ export const teams = {
       link: "https://www.usf.edu/ai-cybersecurity-computing/people/faculty/jing-wang-2.aspx"
     }
   ],
+  Executive: [
+    {
+      name: "Kruthika Rapolu",
+      role: "President",
+      major: "Computer Science",
+      image: "/teams/marketing-director2.png",
+      link: "https://www.linkedin.com/in/kruthika-rapolu-aa8280384/"
+    },
+    {
+      name: "Rodela Ghosh",
+      role: "Vice President",
+      major: "Computer Science",
+      image: "/teams/web-developer.png",
+      link: "https://rodela-dev-site.onrender.com/",
+    }
+  ],
+  Marketing: [
+    { name: "Princess James", role: "Media/Content Lead", major: "Computer Science", image: "/teams/content-lead.png", link:"https://www.linkedin.com/in/princess-as-in-james/" },
+    { name: "Zoe Gongora", role: "Design Co-Lead", major: "Chemical Engineering", image: "/teams/design-lead.png", link: "https://www.linkedin.com/in/zoe-gongora-a41949389/" },
+    { name: "Patcharalak Tulyakul", role: "Social Media Manager", major: "Computer Science", image: "/teams/social-media-manager.png", link: "https://www.linkedin.com/in/patcharalak-tulyakul/" },
+  ],
+  Tech: [
+    { name: "Hana Ahmed", role: "Director of Technology", major: "Computer Science", image: "/teams/technology-director.png", link: "https://linkedin.com/in/hanaahmed-dev" },
+    { name: "Jaydah Barnett", role: "Tech Co-Lead", major: "Biomedical Engineering", image: "/teams/development-director.png", link: "https://www.linkedin.com/in/jaydah-barnett-148949380/" },
+    { name: "Ashly Genao Taveras", role: "Web Developer", major: "Computer Science", image: "/teams/web-developer-2.png", link: "https://www.linkedin.com/in/ashly-genao-taveras/" },
+  ],
+  Outreach: [
+    { name: "Sneha Lama", role: "Industry Outreach Lead", major: "Cybersecurity", image: "/teams/industry-outreach-lead.png", link: "https://www.linkedin.com/in/snehalama/" },
+    { name: "Amber-Joy Nicholas", role: "Professional Development Lead", major: "Information Security", image: "/teams/professional-development-lead.png", link: "http://www.linkedin.com/in/amber-joy-nicholas-792218242" },
+    { name: "Sumayyah Khan", role: "Community Outreach Lead", major: "Cybersecurity", image: "/teams/community-outreach-lead.png", link: "" },
+  ],
+Operations: [
+    { name: "Dhruvi Vekariya", role: "Secretary", major: "Computer Science", image: "/teams/secretary1.png", link:"https://www.linkedin.com/in/dhruvi-vekariya-8856b1353/" },
+    { name: "Twinkle Markana", role: "Treasurer", major: "Computer Science", image: "/teams/treasurer1.png", link:"https://www.linkedin.com/in/twinklemarkana/" },
+    { name: "Kundana Anumakonda", role: "Event Co-Lead", major: "Computer Science", image: "/teams/event-lead.png", link:"https://www.linkedin.com/in/kundana-anumakonda/" }
+  ]
+};
 
+export const pastTeams = {
+  "2025-2026": {
   Executive: [
     {
       name: "Gabbriel McIntosh",
@@ -54,4 +93,6 @@ export const teams = {
     { name: "Sarrat Afzal", role: "Treasurer", major: "Computer Science", image: "/teams/treasurer.png", link:"https://www.linkedin.com/in/sarratafzal/" },
     { name: "Dhruvi Vekariya", role: "Secretary", major: "Computer Science", image: "/teams/secretary.png", link:"https://www.linkedin.com/in/dhruvi-vekariya-8856b1353/" }
   ]
+  }
 };
+
